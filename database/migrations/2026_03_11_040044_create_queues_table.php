1f43fc2381141608817cb55819a623370fe2a4bd
motoric-o <rico.dharmawan198@gmail.com>
2026-03-11 12:11:48 +0700

    database migration progress

---
+++ database/migrations/2026_03_11_040044_create_queues_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('queues', function (Blueprint $table) {
+            $table->foreignId('event_id')->constrained()->onDelete('set null');
+            $table->id();
+            $table->enum('status', ['waiting','canceled', 'completed'])->default('');
+            $table->timestamps();
+            $table->primary(['event_id','id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('queues');
+    }
+};
---
+++ database/migrations/2026_03_11_041255_create_tickets_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('qr_code_hash')->unique();
+            $table->boolean('is_scanned')->default(false);
+            $table->foreignId('order_id')->constrained()->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};

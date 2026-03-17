10378b0356fcfc80bc3090feba2cba22ac557741
Jayden ME <codeaeryl@gmail.com>
2026-03-17 19:35:42 +0700

    Revert "optimized auth.blade.php and register.blade.php"
    This reverts commit 0a4e845633f524ec5b5bbbfd10fd754229c79ea9.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -16,20 +16,8 @@
 
 <body class="flex min-h-screen flex-col bg-gray-50">
 
-<main class="bg-gray-50 dark:bg-gray-900">
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <svg class="w-8 h-8 text-[#e9d5ff] drop-shadow-[0_0_5px_rgba(233,213,255,0.8)] group-hover:text-white transition-colors duration-300" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"></path>
-            </svg>
-            Tixxy
-        </a>
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-                @yield('content')
-            </div>
-        </div>
-    </div>
+<main class="">
+    @yield('content')
 </main>
 </body>
 

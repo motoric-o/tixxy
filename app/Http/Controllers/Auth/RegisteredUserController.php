74f59fbefdc9c34afd9aa8abb44b7b12dc09574b
Jayden ME <codeaeryl@gmail.com>
2026-03-24 13:00:34 +0700

    User Model and Auth Controller to Database adjustments

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,13 +32,15 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'date_of_birth' => ['required', 'date'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password),
+            'date_of_birth' => $request->date_of_birth,
+            'password_hash' => Hash::make($request->password),
         ]);
 
         event(new Registered($user));
--- app/Models/User.php
+++ app/Models/User.php
@@ -36,7 +36,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'date_of_birth',
+        'password_hash',
     ];
 
     /**

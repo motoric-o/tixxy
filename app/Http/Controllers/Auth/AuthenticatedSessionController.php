0a9cfa7b1f8b4441f8f6989357120e7e036a4118
Jayden ME <codeaeryl@gmail.com>
2026-03-24 17:21:55 +0700

    updated routes

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -28,7 +29,13 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect('/');
+        if ($request->user()->role == 'admin') {
+            return redirect()->route('admin.home');
+        } elseif ($request->user()->role == 'organizer') {
+            return redirect()->route('organizer.home');
+        } else {
+            return redirect()->route('home');
+        }
     }
 
     /**
@@ -42,6 +49,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 }

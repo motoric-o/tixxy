d072863f30c067fba4aec0e7e27a37da8fd71ee3
Jayden ME <codeaeryl@gmail.com>
2026-04-09 05:58:00 +0700

    created OrderMail Mailable to mail the order

---
+++ app/Mail/OrderEmail.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Mail;
+
+namespace App\Mail;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+
+class OrderEmail extends Mailable
+{
+    private $order;
+
+    public function __construct(Builder $order) {
+        $this->order = $order;
+    }
+
+    /**
+     * Set the subject and sender details.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Order #' . $this->order->id . ' - Tixxy'
+        );
+    }
+
+    /**
+     * Set the view and data.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.order_email',
+            with: [
+                'order' => $this->order,
+            ],
+        );
+    }
+}
